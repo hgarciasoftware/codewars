a88a01b04809a49df31171d8f33f78ce11caaf52
Hector Garcia <hgarciasoftware@gmail.com>
2022-02-15 19:35:39 -0500

    Add Gilded Rose solution

--- java/6kyu/shop-inventory-manager/Kata.java
+++ java/6kyu/shop-inventory-manager/Kata.java
@@ -1,56 +1,66 @@
 public class ShopInventoryManager {
+  private Item[] items;
 
-	private Item[] items;
+  private static void degrade(Item item) {
+    String name = item.getName().toLowerCase();
+    int sellIn = item.getSellIn();
+    int quality = item.getQuality();
+    int decay = 1;
 
-	public ShopInventoryManager(Item[] items) {
-		this.items = items;
-	}
+    if (quality >= 50 && (name.contains("aged brie") || name.contains("backstage passes"))) {
+      // the quality of an item can never increase beyond 50
+      decay = 0;
+    } else if (name.contains("aged brie") || (name.contains("backstage passes") && sellIn > 10)) {
+      // the "aged brie" goods actually increases in quality each passing day
+      // "backstage passes", like aged brie, increases in quality as it's sell_in value decreases
+      decay = -1;
+    } else if (name.contains("backstage passes") && sellIn <= 10) {
+      // for "backstage passes" quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+      decay = sellIn > 5 ? -2 : -3;
+    }
 
-	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].getName() != "Aged Brie" && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-				if (items[i].getQuality() > 0) {
-					if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-						items[i].setQuality(items[i].getQuality() - 1);
-					}
-				}
-			} else {
-				if (items[i].getQuality() < 50) {
-					items[i].setQuality(items[i].getQuality() + 1);
-					if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-						if (items[i].getSellIn() < 11) {
-							if (items[i].getQuality() < 50) {
-								items[i].setQuality(items[i].getQuality() + 1);
-							}
-						}
-						if (items[i].getSellIn() < 6) {
-							if (items[i].getQuality() < 50) {
-								items[i].setQuality(items[i].getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-			if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-				items[i].setSellIn(items[i].getSellIn() - 1);
-			}
-			if (items[i].getSellIn() < 0) {
-				if (items[i].getName() != "Aged Brie") {
-					if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-						if (items[i].getQuality() > 0) {
-							if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-								items[i].setQuality(items[i].getQuality() - 1);
-							}
-						}
-					} else {
-						items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-					}
-				} else {
-					if (items[i].getQuality() < 50) {
-						items[i].setQuality(items[i].getQuality() + 1);
-					}
-				}
-			}
-		}
-	}
+    if (sellIn < 0 && !name.contains("aged brie")) {
+      // once the sell_in days is less then zero, quality degrades twice as fast
+      decay *= 2;
+    }
+
+    if (name.contains("conjured")) {
+      // "Conjured" items degrade in quality twice as fast as normal items
+      decay *= 2;
+    }
+
+    if (name.contains("backstage passes") && sellIn <= 0) {
+      // for "backstage passes" quality drops to 0 after the concert (sell_in 0 or lower)
+      item.setQuality(0);
+    } else if (!name.contains("sulfuras")) {
+      if (quality - decay >= 50 && decay < 0) {
+        // the quality of an item can never increase beyond 50
+        item.setQuality(50);
+      } else if (quality - decay <= 0) {
+        // the quality of an item can never be negative
+        item.setQuality(0);
+      } else {
+        item.setQuality(quality - decay);
+      }
+    }
+  }
+
+  public ShopInventoryManager(Item[] items) {
+    this.items = items;
+  }
+
+  public void updateQuality() {
+    for (int i = 0; i < items.length; i++) {
+      Item item = items[i];
+      String name = item.getName().toLowerCase();
+
+      // "Sulfuras" goods, being legendary items, never change their sell_in or quality values
+      if (!name.contains("sulfuras")) {
+        // first, setSellIn to getSellIn() - 1
+        item.setSellIn(item.getSellIn() - 1);
+        // then, degrade item accordingly
+        degrade(item);
+      }
+    }
+  }
 }
